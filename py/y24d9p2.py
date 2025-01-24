8badb328435821caef7dfe11dd9f71e626d76e41
ZikXewen <datasup12@gmail.com>
2025-01-24 12:23:11 -0600

    d9

--- py/y24d9p2.py
+++ py/y24d9p2.py
@@ -1,17 +1,16 @@
 from sys import stdin
 
-v = [int(c) * [-1 if i % 2 else (i // 2)] for i, c in enumerate(stdin.read().strip())]
-v = [y for x in v for y in x]
-j = len(v) - 1
-ans = 0
-for i, c in enumerate(v):
-  while v[j] == -1:
-    j -= 1
-  if i > j:
-    break
-  if c != -1:
-    ans += i * c
-  else:
-    ans += i * v[j]
-    j -= 1
-print(ans)
+v = [(int(c), -1 if i % 2 else (i // 2)) for i, c in enumerate(stdin.read().strip())]
+for i in range(len(v))[::-1]:
+  c, x = v[i]
+  if x != -1:
+    for j in range(i):
+      d, y = v[j]
+      if y == -1 and d >= c:
+        v[i] = c, -1
+        v[j] = c, x
+        v.insert(j + 1, (d - c, -1))
+        break
+v = [c * [x] for c, x in v]
+v = [x for y in v for x in y]
+print(sum([(c != -1) * i * c for i, c in enumerate(v)]))
