499fc4556b4e4478cdab23b45226da9868e20861
ZikXewen <datasup12@gmail.com>
2025-01-25 17:10:37 -0600

    d11

---
+++ py/y24d11p1.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+mem = {}
+
+
+def rec(x, i):
+  if i == 0:
+    return 1
+  if x == 0:
+    return rec(1, i - 1)
+  if (x, i) in mem:
+    return mem[(x, i)]
+  mem[(x, i)] = (
+    rec(2024 * x, i - 1)
+    if len(str(x)) % 2
+    else (
+      rec(int(str(x)[: len(str(x)) // 2]), i - 1)
+      + rec(int(str(x)[len(str(x)) // 2 :]), i - 1)
+    )
+  )
+  return mem[(x, i)]
+
+
+print(sum([rec(int(x), 25) for x in stdin.read().strip().split()]))
